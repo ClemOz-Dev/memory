0ccab20c4fa0da19ae09b57befcd88b926db6bb7
ClemOz-Dev <72736732+ClemOz-Dev@users.noreply.github.com>
2021-07-06 12:20:27 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+  
+console.log('Fichier App Chargé');
+const app = {
+
+    init: function () {
+       
+        if (document.getElementById('game-container')){
+           game.init();
+        } 
+
+    },
+
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
